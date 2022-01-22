a5de9738e16c0026eb4b8d7b598b04be5831d65f
stamovm <m.stamov@yahoo.com>
2022-01-21 23:35:54 -0800

    Added a button that dynamicaly creates other buttons

--- prj1/script.js
+++ prj1/script.js
@@ -1,2 +1,21 @@
-let a = 0
+function btn1Clicked() {
+  console.log('clicked')
+  const output = document.getElementById('content1')
+  output.appendChild(renderButton('clicked'))
+}
 
+function renderButton(txt, cls = 'btn') {
+  const btn = document.createElement('button')
+  btn.id = Math.random()
+  btn.textContent = txt
+  btn.classList.add(cls)
+  btn.addEventListener('click', () => btnClick(btn.id))
+  return btn
+}
+
+function btnClick(id) {
+  console.log('clicked ', id)
+  let btn = document.getElementById(id)
+  console.log(btn)
+  btn.innerHTML = id
+}
