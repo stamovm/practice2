35b84ad3b870c08a090d20221ae6ca73584dce06
stamovm <m.stamov@yahoo.com>
2022-01-26 23:40:11 -0800

    practiced setInterval function

--- prj1/script.js
+++ prj1/script.js
@@ -1,21 +1,27 @@
 function btn1Clicked() {
   console.log('clicked')
   const output = document.getElementById('content1')
-  output.appendChild(renderButton('clicked'))
-}
-
-function renderButton(txt, cls = 'btn') {
-  const btn = document.createElement('button')
-  btn.id = Math.random()
-  btn.textContent = txt
-  btn.classList.add(cls)
-  btn.addEventListener('click', () => btnClick(btn.id))
-  return btn
+  output.appendChild(createButton('clicked'))
+  clearInterval(intervalID)
 }
 
 function btnClick(id) {
   console.log('clicked ', id)
   let btn = document.getElementById(id)
   console.log(btn)
-  btn.innerHTML = id
+  btn.textContent = id
+}
+
+let intervalID = setInterval(function () {
+  btn1Clicked()
+}, 1000)
+
+//-----------------------------------------
+function createButton(txt, cls = 'btn') {
+  const btn = document.createElement('button')
+  btn.id = Math.random() * 100
+  btn.textContent = txt
+  btn.classList.add(cls)
+  btn.addEventListener('click', () => btnClick(btn.id))
+  return btn
 }
