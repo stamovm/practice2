21aef6ca15ba53683d1706ee6acc9665b680cdd5
stamovm <m.stamov@yahoo.com>
2022-02-01 20:43:04 -0800

    Created animation and  experemented by moving buttons.

--- prj1/script.js
+++ prj1/script.js
@@ -1,16 +1,24 @@
 'use strict'
-// let button = { id: 1, className: 'btn', txt: 'btn' }
+let button = { id: 1, className: 'btn', txt: 'btn' }
+let buttons = []
 let timer1 = createTimer(btn1Clicked)
-let anim1 = createAnimation(testFunction)
+let anim1 = createAnimation(processButtons)
 
-function testFunction() {
-  // animate
-
-  document.getElementById('content1').appendChild(createButton('clicked'))
+function processButtons(delta) {
+  if (!buttons) return
+  buttons.forEach((btn) => {
+    btn.x += 0.01 * delta
+    btn.btn.style.left = btn.x + 'px'
+    console.log('btn is', btn.btn.style.left, btn.x)
+  })
 }
 
 function btn1Clicked() {
-  document.getElementById('content1').appendChild(createButton('clicked'))
+  let btn = createButton('clicked')
+
+  buttons.push({ x: 0, y: 0, btn: btn })
+
+  document.getElementById('content1').appendChild(btn)
 }
 
 function btn2Clicked() {
@@ -27,16 +35,14 @@ function btn4Clicked() {
 }
 
 function btn5Clicked() {
-  let interval = parseInt(document.getElementById('input1').value)
-  if (interval > 0) anim1.setInterval(interval)
-
-  if (anim1.isRunning) anim1.start()
+  if (!anim1.isRunning()) anim1.start()
+  else anim1.stop()
 }
 
 function btnClick(id) {
-  let btn = document.getElementById(id)
-  console.log(btn)
-  btn.textContent = id
+  // let btn = document.getElementById(id)
+  // btn.style.left = 100 + 'px'
+  // console.log(btn.style.left)
 }
 
 //---------Reusable----Functions-------------------------------
@@ -47,6 +53,7 @@ function createButton(txt, cls = 'btn') {
   btn.id = Math.random() * 100
   btn.textContent = txt
   btn.classList.add(cls)
+  btn.classList.add('btn2')
   btn.addEventListener('click', () => btnClick(btn.id))
   return btn
 }
@@ -79,21 +86,22 @@ function createTimer(func, interval = 1000) {
   }
 }
 
-function createAnimation(func, interval = 33) {
+function createAnimation(func) {
   let running = false
   let animID = 0
-  let int = interval
-  function repeatAnimation() {
-    func()
-    setTimeout(requestAnimationFrame(repeatAnimation), int)
+  let delta = 0
+  let oldTime = 0
+
+  function repeatAnimation(time) {
+    if (oldTime) delta = time - oldTime
+    func(delta)
+    oldTime = time
+    if (running) requestAnimationFrame(repeatAnimation)
   }
+
   return {
     isRunning: function () {
       return running
-    },
-    setInterval: function (interval) {
-      int = interval
-      console.log('🚀 ~ file: script.js ~ line 96 ~ createAnimation ~ int', int)
     },
     start: function () {
       animID = requestAnimationFrame(repeatAnimation)
