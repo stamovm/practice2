1a05cd0a27a486783d38e4ffc7f1e57e9b545fa7
stamovm <m.stamov@yahoo.com>
2022-04-06 23:23:34 -0700

    Updated prj 2 to React 18

--- prj2/src/index.js
+++ prj2/src/index.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+// import React from 'react'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
 import App from './App'
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>,
+//   document.getElementById('root')
+// )
+
+const root = createRoot(document.getElementById('root'))
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 )
